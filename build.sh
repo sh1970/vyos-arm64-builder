2b97524134eac1dee7bd4a2824ca3614a06435d1
mybsd <mybsd@591wifi.com>
2024-06-30 00:55:17 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 BUILD_BY="mybsd@591wifi.com"
 
-#apt-get install -y kpartx make pbuilder devscripts python3-pystache python3-git python3-setuptools parted dosfstools python3-toml python3-jinja2
+#apt-get install -y libgnutls28-dev kpartx make pbuilder devscripts python3-pystache python3-git python3-setuptools parted dosfstools python3-toml python3-jinja2
 #CONTAINER_NAME="vyos/vyos-build:current-arm64"
 ./build-packages.sh
 cd ./vyos-build && ./build-vyos-image --architecture=amd64 --build-by="${BUILD_BY}" --build-type=release iso
